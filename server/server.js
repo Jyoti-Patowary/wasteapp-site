013556708904f9370d30351728a5481aa313a139
Jyoti-Patowary <jpatowary8@gmail.com>
2022-12-07 15:45:27 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config({ path: "./config/config.env" });
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
