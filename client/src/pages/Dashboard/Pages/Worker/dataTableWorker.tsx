f765c1c814ec6ce08fd43763d19b0a0a1775cd1b
Jyoti-Patowary <jpatowary8@gmail.com>
2022-12-07 17:44:09 +0530

    Update dataTableWorker.tsx

--- client/src/pages/Dashboard/Pages/Worker/dataTableWorker.tsx
+++ client/src/pages/Dashboard/Pages/Worker/dataTableWorker.tsx
@@ -39,7 +39,7 @@ export default function WorkerDataTable() {
   }, []);
 
   const fetchData = async () => {
-    let res = await axios.get("https://zero-waste-0yjw.onrender.com//workers", {
+    let res = await axios.get("https://zero-waste-0yjw.onrender.com/workers", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
